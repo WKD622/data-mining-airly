d1bf7183af49b59391e715c8e1b3a6639f78264d
WKD622 <kuba.ziarko@gmail.com>
2020-03-21 12:48:25 +0100

    adds readme

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from src.helpers.database import perform_select_and_save_to_csv
 
-output_columns = ['sensor_id', 'pm10']
+output_columns = ['sensor_id', 'sum(pm10)']
+output_columns_string = ', '.join(output_columns)
 
-command = (f"SELECT sensor_id, sum(pm10) FROM measurements WHERE sensor_id=239 group by sensor_id")
+command = (f"SELECT {output_columns_string} FROM measurements WHERE sensor_id=239 group by sensor_id")
 output_filename = 'result.csv'
 
 perform_select_and_save_to_csv(select_command=command, output_filename=output_filename, columns=output_columns)
